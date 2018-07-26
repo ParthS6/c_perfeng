60f316506934e69a803f5e86b7744e46d148b7fe
ParthS <psavla2@gmail.com>
2018-07-26 19:46:34 -0400

    Create remainderl.c

---
+++ c_files/remainderl.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    long long int count;
+    for( count=0; count <= 1000000; count++) 
+    {
+        remainderl((long double)2,(long double)3);
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("remainderl took %6.9f\n", secs);
+    
+}
